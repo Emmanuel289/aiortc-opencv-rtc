91e596e7f62361df2acd7846af1712f070892617
Emmanuel289 <emmirald@gmail.com>
2023-06-14 12:26:12 -0600

    loading test index page from server

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
-def main():
-    pass
+import os
+from aiohttp import web
+
+ROOT = os.path.dirname(__file__)
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type='text/html', text=content)
 
 
 if __name__ == '__main__':
-    main()
+    app = web.Application()
+    app.router.add_get("/", index)
+    web.run_app(app)
