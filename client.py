8172926ba0b2e3548d7dd32c4b6c9413beb18a1e
Emmanuel289 <emmirald@gmail.com>
2023-06-14 14:41:23 -0600

    Receive offer from server and create an answer

--- client.py
+++ client.py
@@ -1,6 +1,31 @@
-def main():
-    pass
+import asyncio
+from aiortc import RTCPeerConnection
+from aiortc.contrib.signaling import TcpSocketSignaling
 
 
-if __name__ == '__main__':
-    main()
+async def receive_offer(signaling):
+    # Create a peer connection
+    pc = RTCPeerConnection()
+
+    # Receive the offer from the server
+    offer = await signaling.receive()
+
+    # Set the remote description of the peer connection
+    await pc.setRemoteDescription(offer)
+
+    # Create an answer
+    await pc.createAnswer()
+
+
+async def main():
+    # Connect to the server
+    host, port = await asyncio.open_connection(host='0.0.0.0', port=8080)
+
+    # Create a signaling instance for the server
+    signaling = TcpSocketSignaling(host=host, port=port)
+
+    # Receive the offer from the server and send an answer
+    await receive_offer(signaling)
+
+    # Close the signaling connection
+    signaling.close()
--- server.py
+++ server.py
@@ -1,15 +1,48 @@
+import asyncio
 import os
 from aiohttp import web
+from aiortc import RTCPeerConnection
+from aiortc.contrib.signaling import TcpSocketSignaling
 
-ROOT = os.path.dirname(__file__)
+
+async def send_offer(signaling):
+    # Create peer connection
+    pc = RTCPeerConnection()
+
+    # Create an offer
+    offer = await pc.createOffer()
+
+    # Connect signaling
+    await signaling.connect()
+
+    # Set the local description of the peer connection
+    await pc.setLocalDescription(offer)
+
+    # Send offer to client
+    await signaling.send(offer)
 
 
-async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
-    return web.Response(content_type='text/html', text=content)
+async def handle_client(host, port):
+    # Create a signaling instance for the client
+    signaling = TcpSocketSignaling(host=host, port=port)
+
+    # Send the offer to the client
+    send_offer(signaling)
+
+    # Close the signaling connection
+    signaling.close()
+
+
+async def main():
+    server = await asyncio.start_server(handle_client, host='0.0.0.0', port=8080)
+
+    addr = server.sockets[0].getsockname()
+
+    print(f'Server started on {addr[0]}:{addr[1]}')
+
+    async with server:
+        await server.start_serving()
 
 
 if __name__ == '__main__':
-    app = web.Application()
-    app.router.add_get("/", index)
-    web.run_app(app)
+    asyncio.run(main())
