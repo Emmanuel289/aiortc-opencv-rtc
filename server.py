2beb02d4d327bb11c22fc4b97b5918e30411e6f9
Emmanuel289 <emmirald@gmail.com>
2023-06-14 23:06:01 -0600

    Images of the bouncing ball are transmitted to the client at a frequency of 1s

--- server.py
+++ server.py
@@ -3,43 +3,55 @@ import numpy as np
 import cv2
 from aiortc import RTCPeerConnection, MediaStreamTrack
 from aiortc.contrib.signaling import TcpSocketSignaling
+from av import VideoFrame
 
 
-class BallStreamTrack(MediaStreamTrack):
+class BouncingBallTrack(MediaStreamTrack):
+    """
+    A single track of a bouncing ball within a stream.
+    """
+
+    kind = "video"
 
     def __init__(self) -> None:
         super().__init__()
+        self.ball_radius = 20
+        self.ball_color = (0, 255, 0)
+        self.ball_pos_x = 0
+        self.ball_speed = 5
+        self.image_width = 640
+        self.image_height = 480
 
-
-def generate_bouncing_ball():
-    """
-    Generate a continuous 2D image of a ball bouncing  across a screen
-    and send to client
-    """
-
-    ball_radius = 20
-    ball_color = (0, 255, 0)
-    ball_pos_x = 0
-    ball_speed = 5
-    image_width = 640
-    image_height = 480
-
-    while True:
+    async def recv(self) -> VideoFrame:
+        """
+        Receives the next video frame of the bouncing ball
+        """
         # Create a blank image
-        image = np.zeros((image_height, image_width, 3), dtype=np.uint8)
+        image = np.zeros(
+            (self.image_height, self.image_width, 3), dtype=np.uint8)
 
         # Update ball position
-        ball_pos_x += ball_speed
-        if ball_pos_x < 0 or ball_pos_x > image_width - ball_radius:
-            ball_speed *= - 1
+        self.ball_pos_x += self.ball_speed
+        if self.ball_pos_x < 0 or self.ball_pos_x > self.image_width - self.ball_radius:
+            self.ball_speed *= -1
 
         # Draw the ball on the image
-        cv2.circle(image, (ball_pos_x, image_height // 2),
-                   ball_radius, ball_color, -1)
+        cv2.circle(image, (self.ball_pos_x, self.image_height // 2),
+                   self.ball_radius, self.ball_color, -1)
 
         # Convert the image to bytes
         image_bytes = cv2.imencode('.png', image)[1].tobytes()
-        return image_bytes
+
+        # Create a video frame from the image bytes
+        timestamp = self._next_time_stamp()
+        frame = VideoFrame(width=self.image_width, height=self.image_height,
+                           data=image_bytes, timestamp=timestamp)
+
+        return frame
+
+    def _next_time_stamp(self):
+        self.timestamp += 1
+        return self.timestamp
 
 
 async def send_offer(signaling):
@@ -49,30 +61,36 @@ async def send_offer(signaling):
     # Create an offer
     offer = await pc.createOffer()
 
-    # Connect signaling
-    await signaling.connect()
-
     # Set the local description of the peer connection
     await pc.setLocalDescription(offer)
 
     # Send offer to client
     await signaling.send(offer)
 
-    # Send images of the bouncing ball to the client
-    await generate_bouncing_ball(signaling)
 
-    # Delay a while before sending next image
-    await asyncio.sleep(0.01)
-
-    #
-
-
-async def handle_client(host, port):
+async def handle_client(reader, writer):
     # Create a signaling instance for the client
-    signaling = TcpSocketSignaling(host=host, port=port)
+    signaling = TcpSocketSignaling(reader, writer)
 
     # Send the offer to the client
     send_offer(signaling)
+
+    # Create a ball stream track
+    ball_stream_track = BouncingBallTrack()
+
+    # Create a peer connection
+    pc = RTCPeerConnection()
+
+    @pc.on("track")
+    def on_track(track):
+        print(f"Track {track.kind} received")
+
+    # Add the ball to the peer connection
+    pc.addTrack(ball_stream_track)
+
+    # Set the remote description of the peer connection
+    answer = await signaling.receive()
+    await pc.setRemoteDescription(answer)
 
     # Close the signaling connection
     signaling.close()
