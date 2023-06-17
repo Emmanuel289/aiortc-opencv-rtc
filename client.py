6eb2a07f941fb5fffd5c8e4e2a5f559e10cfe6d3
Emmanuel289 <emmirald@gmail.com>
2023-06-17 14:13:53 -0600

    Server transmits ball frames but cv can't load images on client -DEBUG

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import asyncio
 import cv2 as cv
+import numpy as np
 from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, MediaStreamTrack
 from aiortc.contrib.signaling import TcpSocketSignaling, BYE
 from av import VideoFrame
@@ -22,42 +23,47 @@ class ImageDisplayReceiver(MediaStreamTrack):
     def __init__(self, track):
         super().__init__()
         self.track = track
-        app_log.info("track id: %s", track.id)
 
-    def recv(self):
-        frame = self.track.recv()
-        app_log.info('frame is %s', frame)
-        img = frame.to_ndarray(format="bgr24")
-        app_log.info('seen image is %s', img)
-        cv.imshow("Bouncing ball", img)
+    async def recv(self):
+        print('inside recv()')
+        frame = await self.track.recv()
+
+        # Debug -> CV not loading image
+        # image = frame.to_ndarray(format="bgr24")
+        # Create a window and display the image
+        # cv.imshow("Bouncing Ball", image)
+        # cv.waitKey(1)  # Wait until a key is pressed
+        # cv.destroyAllWindows()  # Close the window
+
+        # # Exit if 'q' is pressed
+        # if cv.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
         return frame
 
 
-async def receive_and_display_bouncing_ball(pc, signaling):
-    """
-    Receives and displays images of the bouncing ball
-    """
-
-    app_log.info('Receiving images of bouncing ball...')
+async def display_ball_frames(pc, signaling):
+    print("Display ball frames...")
 
     @pc.on("track")
-    async def on_track(track):
-        app_log.info("Receiving %s" % track.kind)
+    def on_track(track):
+        print("Receiving %s" % track.kind)
         if track.kind == "video":
-            receiver = ImageDisplayReceiver(track)
-            pc.addTrack(receiver)
+            pc.addTrack(ImageDisplayReceiver(track))
 
     # connect signaling
     await signaling.connect()
 
+    # consume signaling
     while True:
         obj = await signaling.receive()
         if isinstance(obj, RTCSessionDescription):
             await pc.setRemoteDescription(obj)
+
             if obj.type == "offer":
                 # send answer
-                answer = await pc.createAnswer()
-                await pc.setLocalDescription(answer)
+                print("Received offer")
+                await pc.setLocalDescription(await pc.createAnswer())
                 await signaling.send(pc.localDescription)
         elif isinstance(obj, RTCIceCandidate):
             await pc.addIceCandidate(obj)
@@ -67,33 +73,50 @@ async def receive_and_display_bouncing_ball(pc, signaling):
 
 
 async def receive_offer_send_answer(pc, signaling):
-    """
-    Receives SDP offer from server peer and sends an ACK SDP message
-    """
-    offer = await signaling.receive()
-    app_log.info('Received offer from server ...')
+    print("Receive Offer and Send Answer")
 
-    await pc.setRemoteDescription(offer)
+    @pc.on("track")
+    def on_track(track):
+        print("Receiving %s" % track.kind)
 
+    # connect signaling
+    await signaling.connect()
+
+    # consume signaling
     while True:
-        obj = await pc.createAnswer()
-        if obj.type == "answer":
-            app_log.info("Created answer for server ")
-            await pc.setLocalDescription(obj)
-            await signaling.send(obj)
-        break
+        obj = await signaling.receive()
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+
+            if obj.type == "offer":
+                # send answer
+                # add_tracks()
+                print("Received offer")
+                answer = await pc.createAnswer()
+                await pc.setLocalDescription(answer)
+                print("Answer sent %s", answer.sdp)
+                await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
 
 
 if __name__ == "__main__":
     signaling = TcpSocketSignaling(HOST_IP, PORT_NO)
+
     peer_connection = RTCPeerConnection()
+
     loop = asyncio.get_event_loop()
+
     try:
+        # loop.run_until_complete(
+        # receive_offer_send_answer(peer_connection, signaling))
         loop.run_until_complete(
-            receive_and_display_bouncing_ball(peer_connection, signaling))
+            display_ball_frames(peer_connection, signaling))
     except KeyboardInterrupt:
         pass
     finally:
         loop.run_until_complete(signaling.close())
         loop.run_until_complete(peer_connection.close())
-        cv.destroyAllWindows()
--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 import asyncio
+import fractions
 from queue import Queue
+import time
 import numpy as np
 import cv2 as cv
-from aiortc import RTCPeerConnection, MediaStreamTrack, RTCSessionDescription
+from aiortc import (
+    RTCPeerConnection,
+    MediaStreamTrack,
+    RTCSessionDescription,
+)
 from aiortc.contrib.signaling import TcpSocketSignaling, BYE
-from av.frame import Frame
 from av import VideoFrame
 from logger import app_log
 
+VIDEO_CLOCK_RATE = 90000
+VIDEO_PTIME = 1 / 30  # 30fps
+VIDEO_TIME_BASE = fractions.Fraction(1, VIDEO_CLOCK_RATE)
 HOST_IP = '127.0.0.1'
 PORT_NO = 8080
+
+# Step 3 Done -> Get and event loop and run coroutine
 
 
 class BouncingBallTrack(MediaStreamTrack):
@@ -20,53 +30,59 @@ class BouncingBallTrack(MediaStreamTrack):
     kind = "video"
 
     def __init__(self):
-        super().__init__()
-        self.ball_radius = 20
-        self.ball_color = (0, 255, 0)
-        self.width = 640
-        self.height = 480
-        self.ball_x = self.width // 2
-        self.ball_y = self.height // 2
-        self.velocity_x = 5
-        self.velocity_y = 5
-        self.timestamp = 0
-        self.queue = Queue(100)
-
-    def generate_frames(self):
-        # Create a blank image frame
-        image = np.zeros((self.height, self.width, 3), dtype=np.uint8)
-        # Draw the ball on the frame
-        cv.circle(image, (self.ball_x, self.ball_y),
-                  self.ball_radius, self.ball_color, -1)
-        # Display the frame
-        cv.imshow("Bouncing Ball", image)
-        while True:
-
-            # Update ball position
-            self.ball_x += self.velocity_x
-            self.ball_y += self.velocity_y
-
-            # Check ball boundaries
-            if self.ball_x + self.ball_radius >= self.width or self.ball_x - self.ball_radius <= 0:
-                self.velocity_x *= -1
-            if self.ball_y + self.ball_radius >= self.height or self.ball_y - self.ball_radius <= 0:
-                self.velocity_y *= -1
-
-            # Enqueue image with updated coordinates
-            app_log.info('image is %s', image)
-            self.queue.put(image)
+        super().__init__()  # did not forget this!
 
     async def recv(self):
-        app_log.info('item in queue is %s', self.queue.get())
-        app_log.info('Receiving the next frame...')
-        image = self.queue.get()
-        app_log.info('image is %s', image)
-        frame = VideoFrame.from_ndarray(image, format='bgr24')
-        app_log.info('Frame is: %s', frame)
-        pts, time_base = await self.next_timestamp()
-        frame.pts = pts
-        frame.time_base = time_base
-        return frame
+        app_log.info('Inside recv()')
+        ball_radius = 10
+        ball_color = (0, 0, 255)
+        ball_speed = 1
+
+        # Define canvas properties
+        canvas_width = 640 * 3
+        canvas_height = 480 * 3
+
+        # Initialize ball position and velocity
+        ball_x = canvas_width // 2
+        ball_y = canvas_height // 2
+        ball_dx = ball_speed
+        ball_dy = ball_speed
+
+        while True:
+            # Create a blank canvas
+            canvas = np.zeros((canvas_height, canvas_width, 3), dtype=np.uint8)
+            canvas.fill(255)
+
+            # Update ball position
+            ball_x += ball_dx
+            ball_y += ball_dy
+
+            # Check if the ball hits the boundaries
+            if ball_x + ball_radius >= canvas_width or ball_x - ball_radius <= 0:
+                ball_dx *= -1  # Reverse horizontal velocity
+            if ball_y + ball_radius >= canvas_height or ball_y - ball_radius <= 0:
+                ball_dy *= -1  # Reverse vertical velocity
+
+            # Draw the ball on the canvas
+            cv.circle(canvas, (ball_x, ball_y), ball_radius, ball_color, -1)
+
+            frame = VideoFrame.from_ndarray(canvas, format="bgr24")
+
+            pts, time_base = await self.next_timestamp()
+            frame.pts = pts
+            frame.time_base = time_base
+            return frame
+
+    async def next_timestamp(self):
+        if hasattr(self, "_timestamp"):
+            self._timestamp += int(VIDEO_PTIME * VIDEO_CLOCK_RATE)
+            wait = self._start + (self._timestamp /
+                                  VIDEO_CLOCK_RATE) - time.time()
+            await asyncio.sleep(wait)
+        else:
+            self._start = time.time()
+            self._timestamp = 0
+        return self._timestamp, VIDEO_TIME_BASE
 
 
 async def send_offer_receive_answer(pc, signaling):
@@ -74,19 +90,18 @@ async def send_offer_receive_answer(pc, signaling):
     Creates an SDP offer and sends to client peer. Also receives answer 
     from client and logs an ACK message
     """
-    await signaling.connect()  # connect signaling
-
-    pc = RTCPeerConnection()
     bouncing_ball = BouncingBallTrack()
 
     pc.addTrack(bouncing_ball)  # add bouncing ball media track
 
+    # connect signaling
+
+    await signaling.connect()
+
     # create offer and send
-    app_log.info('Creating offer...')
     offer = await pc.createOffer()
     app_log.info('Offer created ...')
     await pc.setLocalDescription(offer)
-    app_log.info('Offer sent ...')
     await signaling.send(pc.localDescription)
 
     # consume signaling
@@ -96,41 +111,19 @@ async def send_offer_receive_answer(pc, signaling):
             await pc.setRemoteDescription(obj)
 
             if obj.type == "answer":
-                answer = RTCSessionDescription(sdp=obj.sdp, type='answer')
-            app_log.info(
-                "Answer received ...")
-            break
-
-
-async def send_bouncing_ball(pc, signaling):
-    """
-    Send images of the bouncing ball to the client peer
-    """
-    app_log.info('Sending images of the bouncing ball ...')
-
-    sender = BouncingBallTrack()
-    pc.addTrack(sender)
-
-    await signaling.connect()
-
-    await pc.setLocalDescription(await pc.createOffer())
-    await signaling.send(pc.localDescription)
-
-    while True:
-        obj = await signaling.receive()
-        if isinstance(obj, RTCSessionDescription):
-            await pc.setRemoteDescription(obj)
-        elif obj is BYE:
-            app_log.info("Exiting ...")
-            break
+                app_log.info(
+                    "Answer %s", obj.sdp)
 
 if __name__ == "__main__":
     signaling = TcpSocketSignaling(HOST_IP, PORT_NO)
+
     peer_connection = RTCPeerConnection()
+
     loop = asyncio.get_event_loop()
+
     try:
         loop.run_until_complete(
-            send_bouncing_ball(peer_connection, signaling))
+            send_offer_receive_answer(peer_connection, signaling))
     except KeyboardInterrupt:
         pass
     finally:
